1ecfd1d51b5eb4ac12f75b9bf56f87635a57c0e0
wtf <wtf@163.com>
2025-07-01 00:06:14 +0800

    add noise impl

--- utils.py
+++ utils.py
@@ -100,3 +100,91 @@ def extract(code):
         inline_comment_count=visitor.inline_comment_count,
         inter_line_comment_count=visitor.inter_line_comment_count,
     )
+
+def comment_idx(code:str):
+    code_split = code.split('\n')
+    double_quote_start = -1
+    double_quote_end = -1
+    single_quote_start = -1
+    single_quote_end = -1
+    length = len(code_split)
+    comment_lines = []
+    for i in range(length):
+        cur_line = code_split[i].strip()
+        if cur_line.startswith("#"):
+            comment_lines.append(i)
+        elif '#' in cur_line:
+            code_split[i] = code_split[i].split('#')[0]
+        if cur_line.startswith("\'\'\'") and single_quote_start == -1:
+            single_quote_start = i
+            if cur_line.count("\'\'\'") == 1:
+                continue
+        if cur_line.endswith("\'\'\'") and single_quote_start != -1:
+            single_quote_end = i
+            comment_lines.extend(list(range(single_quote_start,single_quote_end+1)))
+            single_quote_start = -1
+            single_quote_end = -1
+        if cur_line.startswith('\"\"\"') and double_quote_start == -1:
+            double_quote_start = i
+            if cur_line.count('\"\"\"') == 1:
+                continue
+        if cur_line.endswith('\"\"\"') and double_quote_start != -1:
+            double_quote_end = i
+            comment_lines.extend(list(range(double_quote_start,double_quote_end+1)))
+            double_quote_start = -1
+            double_quote_end = -1
+    return comment_lines
+
+def generate_random_string(length=5):
+    characters = string.ascii_letters + string.digits
+    # 随机选择字符并生成字符串
+    random_string = ''.join(random.choices(characters, k=length))
+    return random_string
+
+def remove_comments(code:str):
+    code_split = code.split('\n')
+    length = len(code_split)
+    comment_lines = comment_idx(code)
+    code_list = [code_split[i] for i in range(length) if i not in comment_lines]
+
+    return '\n'.join(code_list)
+        
+        
+def change_indent(code:str,updated_indent:int=1,default_indent='    '):
+    updated_indent = random.randint(1,3)
+    code_split = code.split('\n')
+    # 当前缩进长度
+    indent_length = default_indent.count(' ')
+    length = len(code_split)
+    for i in range(length):
+        # 当前行的缩进
+        cur_indent = len(code_split[i]) - len(code_split[i].lstrip())
+        if cur_indent == 0:
+            continue
+        # 当前缩进几次
+        ind_cnt = cur_indent // indent_length
+        # 替换后的缩进长度
+        new_indent = ' ' * ind_cnt * updated_indent 
+        code_split[i] = new_indent + code_split[i].lstrip()
+
+    return '\n'.join(code_split)
+
+def indent_error(code:str,cur_indent='    '):
+    code_split = code.split('\n')
+    max_try = 10
+    comment_lines = comment_idx(code)
+    length = len(code_split)
+    length_id = list(range(length))
+    selected = random.choice(length_id)
+    while selected in comment_lines or code_split[selected] == '':
+        length_id.remove(selected)
+        selected = random.choice(length_id)
+        max_try -= 1
+        if max_try == 0:
+            break
+    r = random.randint(0,100)
+    if r % 2 == 0:
+        code_split[selected] = code_split[selected].lstrip()
+    else:
+        code_split[selected] = cur_indent + code_split[selected]
+    return '\n'.join(code_split)
